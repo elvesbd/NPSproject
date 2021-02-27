5b73cc20c19898af576e5425d29caf564ab1b384
elves brito <elvesbd@yahoo.com>
2021-02-26 23:01:37 -0300

    validate surveys data

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { SurveysRepository } from '../repositories/SurveysRepository';
+import * as yup from 'yup';
+import { AppError } from '../errors/ApError';
 
 class SurveysController {
   async create(req: Request, res: Response) {
     const { title, description } = req.body;
+
+    const schema = yup.object().shape({
+      title: yup.string().required(),
+      description: yup.string().required()
+    });
+
+    try {
+      await schema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+      throw new AppError(err);
+    };
 
     const surveysRepository = getCustomRepository(SurveysRepository);
 
